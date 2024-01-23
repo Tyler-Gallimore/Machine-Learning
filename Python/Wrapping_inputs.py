07aea8df35727f1c93b4a13f412ae7725a0f5741
Tyler-Gallimore <lifforn@mailfence.com>
2024-01-23 05:43:13 -0500

    Moved network to own file

--- Python/Wrapping_inputs.py
+++ Python/Wrapping_inputs.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 # Wrapping the vectors in NumPy arrays
-input_vector = np.array([1.66, 1.56])
+input_vector = np.array([2, 1.5])
 weights_1 = np.array([1.45, -0.66])
 bias = np.array([0.0])
 
@@ -15,4 +15,28 @@ def make_prediction(input_vector, weights, bias):
 
 prediction = make_prediction(input_vector, weights_1, bias)
 
-print(f"The prediction result is:  {prediction}")
+target = 0
+
+mse = np.square(prediction - target)
+
+derivative = 2 * (prediction - target)
+
+weights_1 = weights_1 - derivative
+
+prediction = make_prediction(input_vector, weights_1, bias)
+
+error = (prediction - target) ** 2
+
+def sigmoid_deriv(x):
+    return sigmoid(x) * (1-sigmoid(x))
+
+derror_dprediction = 2 * (prediction - target)
+layer_1 = np.dot(input_vector, weights_1) + bias
+dprediction_dlayer1 = sigmoid_deriv(layer_1)
+dlayer1_dbias = 1
+
+derror_dweights = (
+    derror_dprediction * dprediction_dlayer1 * dlayer1_dbias
+)
+
+print(f"Prediction:  {prediction}, Error:  {error}")
